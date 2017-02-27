e7a7916081c562b64933b75dbf160d92b2002cb2
naveendotc <ncherupally@gmail.com>
2017-02-27 05:40:32 -0800

    created-project

---
+++ web-monitor/myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/naveen/Dropbox/Air-Quality-Monitoring-CS-671-/web-monitor/myenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
